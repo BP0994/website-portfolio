c29d62877bef8d8c8fa3005b9f6864a5948c0f98
BHUPENDRA PAL <118584732+BP0994@users.noreply.github.com>
2023-11-07 02:58:17 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// <!-- Element to contain animated typing -->
+
+
+// <!-- Load library from the CDN -->
+<script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"></script>
+
+// <!-- Setup and start animation! -->
+
+  var typed = new Typed('#element', {
+    strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+    typeSpeed: 50,
+  });
+
